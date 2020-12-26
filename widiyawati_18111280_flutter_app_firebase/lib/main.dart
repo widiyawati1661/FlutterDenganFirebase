89f618ebea81f8913f1ccaacb8570f6cc824e907
widiyawati1661 <59183891+widiyawati1661@users.noreply.github.com>
2020-12-26 11:32:09 +0700

    menyambungkan ke firebase

--- widiyawati_18111280_flutter_app_firebase/lib/main.dart
+++ widiyawati_18111280_flutter_app_firebase/lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:async';
-import 'myScreen.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'dart:async';
+//import 'myScreen.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Aplikasi CRUD',
-        theme: new ThemeData(primarySwatch: Colors.blue),
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
         home: HomePage());
   }
 }
 
 class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  /*final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   final GoogleSignIn googleSignin = new GoogleSignIn();
 
   Future<FirebaseUser> _signIn() async {
@@ -39,13 +46,35 @@ class _HomePageState extends State<HomePage> {
     return Navigator.of(context).push(new MaterialPageRoute(
         builder: (BuildContext context) =>
             new MyScreen(user: firebaseUser, googleSignIn: googleSignin)));
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder(
+            future: Firestore.instance.collection('user').getDocuments(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.blue,
+                  ),
+                );
+              }
+              return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data.documents[index]['Catatan']),
+                    subtitle: Text(snapshot.data.documents[index]['daftar']),
+                  );
+                },
+              );
+
+              /*Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
@@ -57,29 +86,29 @@ class _HomePageState extends State<HomePage> {
                 alignment: Alignment.center,
               ),
             ),
-            Center(
+           body: Center(
                 child: Text(
-              'Copyright by Widiyawati 2020',
-              style: TextStyle(
+                'Copyright by Widiyawati 2020',
+                style: TextStyle(
                   color: Colors.black, height: 20, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            )),
-            //new Padding(padding: const EdgeInsets.only(bottom: 20)),
-            //supaya gambar yang disentuh bisa fungsi
-            new InkWell(
-              onTap: () {
-                _signIn();
-              },
-              child: Image.asset(
-                "images/logo.png",
-                width: 200,
-                height: 200,
-                alignment: Alignment.center,
+                  textAlign: TextAlign.center,)
+                ),
+                new Padding(padding: const EdgeInsets.only(bottom: 20)),
+                //supaya gambar yang disentuh bisa fungsi
+                new InkWell(
+                  onTap: () {
+                    _signIn();
+                  },
+                  child: Image.asset(
+                    "images/logo.png",
+                    width: 200,
+                    height: 200,
+                    alignment: Alignment.center,
+                ),
               ),
-            ),
+            );
           ],
-        ));
+        );*/
+            }));
   }
 }
-
-class SignInWithGoogle {}
